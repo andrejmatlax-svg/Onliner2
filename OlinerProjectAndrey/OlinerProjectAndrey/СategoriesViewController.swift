8afe9657300642ef2bea07b606fe29be0545965a
Andrey <Matlah>
2026-07-23 19:20:10 +0300

    Create СategoriesViewController

---
+++ OlinerProjectAndrey/OlinerProjectAndrey/СategoriesViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  OlinerProjectAndrey
+//
+//  Created by Андрей Матлах on 23.07.2026.
+//
+
+import UIKit
+
+class СategoriesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUiElements()
+    }
+    
+    private func setupUiElements(){
+        setupView()
+        setupHearachy()
+        setupLayout()
+    }
+    
+    private func setupView(){
+        
+    }
+    
+    
+    private func setupHearachy(){
+        
+    }
+    
+    
+    private func setupLayout(){
+        NSLayoutConstraint.activate([
+            
+            
+        ])
+        
+    }
+    
+
+
+}
+
